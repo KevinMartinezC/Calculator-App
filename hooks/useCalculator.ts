afc76589fe72fa66e3afe1a3eebbbc5d2375cf3f
kevinmartinezc <ceronk003@gmail.com>
2025-02-03 10:15:37 -0600

    feat: add validations

--- hooks/useCalculator.ts
+++ hooks/useCalculator.ts
@@ -1,35 +1,60 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react";
 
 enum Operator {
-    add = '+',
-    subtract = '-',
-    multiply = 'x',
-    devide = '%'
-
+  add = "+",
+  subtract = "-",
+  multiply = "x",
+  devide = "%",
 }
 export const useCalculator = () => {
-    const [formula, setFormula] = useState('')
-    const [number, setNumber] = useState('0')
-    const [prevNumber, setPrevNumber] = useState('0')
+  const [formula, setFormula] = useState("");
+  const [number, setNumber] = useState("0");
+  const [prevNumber, setPrevNumber] = useState("0");
 
-    useEffect(() => {
-        //Todo calcular subresultado
-        setFormula(number)
-    }, [number] )
-    
-    const buildNumber = (numberString: string) =>{
-        console.log(numberString)
+  useEffect(() => {
+    //Todo calcular subresultado
+    setFormula(number);
+  }, [number]);
+
+  const buildNumber = (numberString: string) => {
+    //Verificar si ya existe el . decimal
+    if (number.includes(".") && numberString === ".") return;
+
+    if (number.startsWith("0") || number.startsWith("-0")) {
+      if (numberString === ".") {
+        return setNumber(number + numberString);
+      }
+
+      //Evaluar si es otro cero y no hay punto
+
+      if (numberString === "0" && number.includes(".")) {
+        return setNumber(number + numberString);
+      }
+
+      //Evaluar si es diferente de cero, no hay punto y es el primer numero
+
+      if (numberString != "0" && !number.includes(".")) {
+        return setNumber(numberString);
+      }
+
+      // Evitar el 0000.0
+      if (numberString === "0" && !number.includes(".")) {
+        return;
+      }
     }
 
-    const lastOperation = useRef<Operator>()
+    setNumber(number + numberString);
+  };
 
-    return {
-        //props
-        formula,
-        number,
-        prevNumber,
+  const lastOperation = useRef<Operator>();
 
-        //methods
-        buildNumber
-    }
-}
+  return {
+    //props
+    formula,
+    number,
+    prevNumber,
+
+    //methods
+    buildNumber,
+  };
+};
